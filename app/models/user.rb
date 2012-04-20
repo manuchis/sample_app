2b96b8781e79e3c84dcadd0658fe8f1b1d7b35af
Manuel Portela <manuchis@gmail.com>
2012-04-20 02:58:41 -0300

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,15 @@ class User < ActiveRecord::Base
     email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     has_many :microposts, :dependent => :destroy
-
+    has_many :relationships, :foreign_key => "follower_id",
+                             :dependent => :destroy
+    has_many :following, :through => :relationships, :source => :followed
+    has_many :reverse_relationships, :foreign_key => "followed_id",
+                                       :class_name => "Relationship",
+                                       :dependent => :destroy
+      has_many :followers, :through => :reverse_relationships, :source => :follower
+      
+      
    validates :name,  :presence => true,
                      :length   => { :maximum => 50 }
    validates :email, :presence   => true,
@@ -30,8 +38,7 @@ class User < ActiveRecord::Base
      
      
      def feed
-         # This is preliminary. See Chapter 12 for the full implementation.
-         Micropost.where("user_id = ?", id)
+         Micropost.from_users_followed_by(self)
        end
        
      def has_password?(submitted_password)
@@ -47,7 +54,17 @@ class User < ActiveRecord::Base
           (user && user.salt == cookie_salt) ? user : nil
         end   
         
-        
+        def following?(followed)
+          relationships.find_by_followed_id(followed)
+        end
+
+        def follow!(followed)
+          relationships.create!(:followed_id => followed.id)
+        end
+
+        def unfollow!(followed)
+          relationships.find_by_followed_id(followed).destroy
+        end
     
        private
 
